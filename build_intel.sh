458651b5bef6776de8a875827c80d6b3f76e8e20
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-05 11:55:32 -0500

    debugging

--- build_intel.sh
+++ build_intel.sh
@@ -9,8 +9,15 @@ export CC=icc CXX=icpc
 
 [[ $1 == -k ]] && CLEAN=0 || CLEAN=1 
 
+BUILDLAPACK95=0
+BUILDMETIS=0
+BUILDSCOTCH=0
+
 ## LAPACK95   (N.B. included in Intel MKL)
-(cd LAPACK95/
+(
+[[ $BUILDLAPACK95 != 1 ]] && exit
+
+cd LAPACK95/
 
 [[ $CLEAN == 1 ]] && make clean -C SRC
 
@@ -18,21 +25,28 @@ make double -C SRC FC=mpiifort OPTS0="-O3 -fPIC -fno-trapping-math"
 )
 
 ## METIS
-(cd metis
+(
+[[ $BUILDLAPACK95 != 1 ]] && exit
+
+cd metis
 
 [[ $CLEAN == 1 ]] && { make clean; make config CC=icc CXX=icpc; }
 
-make
+make -j -l4 FC=mpiifort
 )
 
 ## Scotch
-(cd scotch/src
+(
+[[ $BUILDSCOTCH != 1 ]] && exit
+
+
+cd scotch/src
 [[ $CLEAN == 1 ]] && { make clean; cd esmumps; make clean; cd ..; }
 
 make FC=mpiifort CC=icc CCS=icc CCD=icc CCP=mpiicc CXX=icpc
 
 cd esmumps
-make FC=mpiifort CC=icc CCS=icc CCD=icc CCP=mpiicc CXX=icpc
+make -j -l4 FC=mpiifort CC=icc CCS=icc CCD=icc CCP=mpiicc CXX=icpc
 )
 
 ## Scalapack is included with Intel Fortran
@@ -45,7 +59,7 @@ SCALAP='-L$(SCALAPDIR) -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_intel_thread 
 
 if [[ -f /etc/redhat-release ]]; then
 
-  make s d FC=mpiifort CC=icc CXX=icpc \
+  make d FC=mpiifort CC=icc CXX=icpc \
      LSCOTCHDIR=../../scotch/lib ISCOTCH=-I../../scotch/include \
      INCPAR=-I/share/pkg/openmpi/3.0.0_intel-2018/install1/include/ \
      LMETISDIR=/share/pkg/metis/5.1.0/install/lib IMETIS=-I/share/pkg/metis/5.1.0/install/include \
@@ -54,12 +68,11 @@ if [[ -f /etc/redhat-release ]]; then
 
 else
 
-  make s d FC=mpiifort CC=icc CXX=icpc \
-     LSCOTCHDIR=../../scotch/lib ISCOTCH=-I../../scotch/include \
-     INCPAR=-I$HOME/intel/compilers_and_libraries/linux/mpi/intel64/include/ \
-     LMETISDIR=../../metis/libmetis IMETIS=-I../../metis/include \
+  make d FC=mpiifort CC=icc CXX=icpc \
      SCALAPDIR=$MKLROOT \
-     SCALAP=$SCALAP
-
+     SCALAP=$SCALAP ]
+     INCPAR=-I$HOME/intel/compilers_and_libraries/linux/mpi/intel64/include/ # don't remove this line!
+     #LSCOTCHDIR=../../scotch/lib ISCOTCH=-I../../scotch/include \
+     #LMETISDIR=../../metis/libmetis IMETIS=-I../../metis/include \
 fi     
 )
