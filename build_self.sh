17b73c08811ed39b8af9be26801e09ea6b95029c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-01-24 11:28:07 -0500

    parameterize build_self [skip ci]
    typo [skip ci]

--- build_self.sh
+++ build_self.sh
@@ -3,17 +3,26 @@
 # builds libraries for Gfortran
 # Note: Currently, Scalapack appears to be incompatible with OpenMPI 4.0. Try OpenMPI 3.1
 
+set -e
+set -u
+
+# disables Intel compiler from interfering
 MKLROOT=
 
-export MPIROOT=$HOME/.local/openmpi-3.1-gcc7/
-export LAPACKROOT=$HOME/.local/lapack-gcc7/
-export SCALAPACKROOT=$HOME/.local/scalapack-gcc7/
-export MUMPSROOT=$HOME/.local/mumps-gcc7/
+PREFIX=$1
+SUFFIX=$2
+MPIPREFIX=$3
+LAPACKPREFIX=$4
 
-export FC=$MPIROOT/bin/mpif90
-export CC=$MPIROOT/bin/mpicc
+[[ -d $PREFIX ]] || { "Install directory $PREFIX does not exist"; exit 1; }
+[[ -d $MPIPREFIX ]] || { "MPI directory $MPIPREFIX does not exist"; exit 1; }
+[[ -d $LAPACKPREFIX ]] || { "LAPACK directory $LAPACKPREFIX does not exist"; exit 1; }
 
-set -e
+export SCALAPACKPREFIX=$PREFIX/scalapack-$SUFFIX/
+export MUMPSPREFIX=$PREFIX/mumps-$SUFFIX/
+
+export FC=$MPIPREFIX/bin/mpif90
+export CC=$MPIPREFIX/bin/mpicc
 
 [[ $1 == -k ]] && CLEAN=0 || CLEAN=1
 
@@ -30,7 +39,7 @@ BUILDSCALAPACK=1
 
 cd scalapack/build
 
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=$SCALAPACKROOT -DMPI_ROOT=$MPIROOT -DLAPACK_ROOT=$LAPACKROOT ..
+cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=$SCALAPACKPREFIX -DMPI_ROOT=$MPIPREFIX -DLAPACK_ROOT=$LAPACKPREFIX ..
 
 cmake --build -j . --target install -- -l 4
 )
@@ -46,15 +55,15 @@ cmake --build -j . --target install -- -l 4
 # LAPACK=  BLAS=
 
 make s d FC=$FC FL=$FC CC=$CC \
-LAPACK=$LAPACKROOT BLAS=$LAPACKROOT \    
+LAPACK=$LAPACKPREFIX BLAS=$LAPACKPREFIX \
 LSCOTCHDIR= ISCOTCH= \
 LMETISDIR= IMETIS= \
-SCALAPDIR=$SCALAPACKROOT \
+SCALAPDIR=$SCALAPACKPREFIX \
 SCALAP='-L$(SCALAPDIR) -lscalapack' \
 ORDERINGSF=-Dpord
 
 # Mumps makefile doesn't have an "install" target so let's do it ourself.
-mkdir -p $MUMPSROOT/{lib,include}
-cp lib/{libpord,lib{s,d}mumps,libmumps_common}.a $MUMPSROOT/lib
-cp include/{s,d}mumps*.h include/mumps_compat.h $MUMPSROOT/include/
+mkdir -p $MUMPSPREFIX/{lib,include}
+cp lib/{libpord,lib{s,d}mumps,libmumps_common}.a $MUMPSPREFIX/lib
+cp include/{s,d}mumps*.h include/mumps_compat.h $MUMPSPREFIX/include/
 )
